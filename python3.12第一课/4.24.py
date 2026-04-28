bf8b278e59a91e251e7e8e96929e48bafb86e8b1
xxlhcvv <2407978636@qq.com>
2026-04-28 20:45:14 +0800

    提交代码

--- python3.12第一课/4.24.py
+++ python3.12第一课/4.24.py
@@ -33,3 +33,16 @@ del cat3
 print(cat2.name,cat2.age)
 #对象属性的私有化，不能单独访问数据属性
 #在属性前面添加双下画线，在类内部使用属性是也要有双下画线
+
+
+class car:
+    def __init__(self, newWheelNum, newColor):
+        self.newWheelNum = newWheelNum
+        self.newColor = newColor
+    def run (self):
+        print("车在跑，目标:夏威夷。")
+    def __del__(self):
+        print("---析构方法被调用---")
+BMW=car(4,"黑色")
+print('车轮数:%d,颜色:%s'%(BMW.newWheelNum,BMW.newColor))
+BMW.run()
--- python第三方文件/练习1.py
+++ python第三方文件/练习1.py
@@ -1,12 +1,1 @@
-class car:
-    def __init__(self, newWheelNum, newColor):
-        self.newWheelNum = newWheelNum
-        self.newColor = newColor
-    def run (self):
-        print("车在跑，目标:夏威夷。")
-    def __del__(self):
-        print("---析构方法被调用---")
-BMW=car(4,"黑色")
-print('车轮数:%d,颜色:%s'%(BMW.newWheelNum,BMW.newColor))
-BMW.run()
 
