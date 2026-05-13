2131073f85163dc8981d319ebe7fff3ab3e31957
xxlhcvv <2407978636@qq.com>
2026-05-13 11:53:27 +0800

    提交代码

---
+++ python3.12第一课/5.13.py
@@ -0,0 +1,38 @@
+#读写csv文件
+#导入csv模块
+import csv
+#两种方式进行读取，csv.reader数据保存为列表，csv.DictReader字典
+with open('./data/carsales.csv','r',encoding='gbk') as f:
+    reader = csv.reader(f)
+    carsales=[carsales_item for carsales_item in reader]
+    print(carsales)
+    print(carsales[2])#列表优势：针对获取某一行数据
+
+with open('./data/carsales.csv','r',encoding='gbk') as f:
+    reader = csv.DictReader(f)
+    #carsales1=[carsales_item for carsales_item in reader]
+    #print(carsales1)
+    column=[carsales_item['汽车型号'] for carsales_item in reader]
+    print(column)#字典优势，针对获取某一列数据
+
+#将数据写入csv文件
+#writerow函数将数据逐行写入csv文件
+with open('./data/test1.csv','w',newline='') as f:
+    writer_csv = csv.writer(f)
+    for carsales_item in carsales:#列表元素为一行数据，所以需遍历
+        writer_csv.writerow(carsales_item)
+#对于字典形式数据，csv.DictWriter类将数据写入CSV文件
+
+with open('./data/carsales.csv', 'r', encoding='gbk') as f:
+    reader = csv.DictReader(f)
+    carsales1 = [carsales_item for carsales_item in reader]
+    print(carsales1)
+
+my_key = []  # 键的集合
+for i in carsales1[0].keys():
+    my_key.append(i)
+with open('./data/test2.csv', 'w', newline = '') as f:
+    write_csv = csv.DictWriter(f, my_key)
+    write_csv.writeheader()  # 输入标题
+    write_csv.writerows(carsales1)  # 输入数据
+
---
+++ python3.12第一课/练习1.py
@@ -0,0 +1,35 @@
+import csv
+height_list = []
+weight_list = []
+with open('./data/height_weight.csv', 'r', encoding='utf-8') as f:
+    reader = csv.DictReader(f)
+    for row in reader:
+        # 步骤2：字符串转浮点数
+        height = float(row['身高/cm'])
+        weight = float(row['体重/kg'])
+        height_list.append(height)
+        weight_list.append(weight)
+def mean(data):
+    return sum(data) / len(data)
+
+def variance(data):
+    m = mean(data)
+    return sum((x - m) ** 2 for x in data) / len(data)
+
+height_mean = round(mean(height_list), 2)
+height_var = round(variance(height_list), 2)
+weight_mean = round(mean(weight_list), 2)
+weight_var = round(variance(weight_list), 2)
+result_data = [
+    {'字段': '身高/cm', '均值': height_mean, '方差': height_var},
+    {'字段': '体重/kg', '均值': weight_mean, '方差': weight_var}
+]
+with open('./data/result_mean_var.csv', 'w', newline='', encoding='gbk') as f:
+    writer = csv.DictWriter(f, fieldnames=['字段', '均值', '方差'])
+    writer.writeheader()
+    writer.writerows(result_data)
+
+print("身高均值:", height_mean)
+print("身高方差:", height_var)
+print("体重均值:", weight_mean)
+print("体重方差:", weight_var)
